1eda7da8d7ceaf57aa15e1d1362c85ff62f9db14
egenerse <egenerse@gmail.com>
2024-11-09 11:59:14 +0100

    feat: movable

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect, useCallback } from "react";
 import { RootState } from "../store";
-import { addElement } from "../store/canvasSlice";
+import { addElement, updateElementPosition } from "../store/canvasSlice";
+import { throttle } from "lodash";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+
+const throttledUpdatePosition = throttle((dispatch, id, x, y) => {
+  dispatch(updateElementPosition({ id, x, y }));
+}, 50);
 
 const Canvas: React.FC = () => {
-  const dispatch = useDispatch();
-  const elements = useSelector((state: RootState) => state.canvas.elements);
+  const dispatch = useAppDispatch();
+  const elements = useAppSelector((state: RootState) => state.canvas.elements);
+
+  const [draggingElement, setDraggingElement] = useState<{
+    id: string;
+    offsetX: number;
+    offsetY: number;
+  } | null>(null);
 
   const handleDrop = (e: React.DragEvent) => {
     e.preventDefault();
@@ -19,14 +30,60 @@ const Canvas: React.FC = () => {
     }
   };
 
+  const handleMouseDown = (e: React.MouseEvent, id: string) => {
+    const element = elements.find((el) => el.id === id);
+    if (element) {
+      const offsetX = e.clientX - element.x;
+      const offsetY = e.clientY - element.y;
+      setDraggingElement({ id, offsetX, offsetY });
+    }
+  };
+
+  const handleMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (draggingElement) {
+        const x = e.clientX - draggingElement.offsetX;
+        const y = e.clientY - draggingElement.offsetY;
+        throttledUpdatePosition(dispatch, draggingElement.id, x, y);
+      }
+    },
+    [draggingElement, dispatch]
+  );
+
+  const handleMouseUp = () => {
+    setDraggingElement(null);
+  };
+
+  useEffect(() => {
+    if (draggingElement) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [draggingElement, handleMouseMove]);
+
   return (
-    <div className="canvas" onDrop={handleDrop} onDragOver={(e) => e.preventDefault()}>
+    <div
+      className="canvas"
+      onDrop={handleDrop}
+      onDragOver={(e) => e.preventDefault()}
+    >
       {elements.map((el) => (
         <div
           key={el.id}
           className={`canvas-item ${el.type}`}
-          style={{ left: el.x, top: el.y }}
-        />
+          style={{ left: el.x, top: el.y, position: "absolute" }}
+          onMouseDown={(e) => handleMouseDown(e, el.id)}
+        >
+          {el.type}
+        </div>
       ))}
     </div>
   );
--- src/store/canvasSlice.ts
+++ src/store/canvasSlice.ts
@@ -8,22 +8,33 @@ type CanvasElement = {
   y: number;
 };
 
+type Path = {
+  id: string;
+  startElementId: string;
+  startPort: string;
+  endElementId: string;
+  endPort: string;
+};
+
 interface CanvasState {
   elements: CanvasElement[];
+  paths: Path[];
+  currentConnection: { startElementId: string; startPort: string } | null;
+  tempConnection: { x: number; y: number } | null;
 }
 
 const initialState: CanvasState = {
   elements: [],
+  paths: [],
+  currentConnection: null,
+  tempConnection: null,
 };
 
 const canvasSlice = createSlice({
   name: "canvas",
   initialState,
   reducers: {
-    addElement: (
-      state,
-      action: PayloadAction<{ type: string; x: number; y: number }>
-    ) => {
+    addElement: (state, action: PayloadAction<{ type: string; x: number; y: number }>) => {
       const newElement = {
         id: uuidv4(),
         type: action.payload.type,
@@ -32,8 +43,41 @@ const canvasSlice = createSlice({
       };
       state.elements.push(newElement);
     },
+    updateElementPosition: (state, action: PayloadAction<{ id: string; x: number; y: number }>) => {
+        const element = state.elements.find((el) => el.id === action.payload.id);
+        if (element) {
+          element.x = action.payload.x;
+          element.y = action.payload.y;
+        }
+      },
+    startConnection: (state, action: PayloadAction<{ elementId: string; port: string }>) => {
+      state.currentConnection = { startElementId: action.payload.elementId, startPort: action.payload.port };
+      state.tempConnection = null;
+    },
+    completeConnection: (state, action: PayloadAction<{ elementId: string; port: string }>) => {
+      if (state.currentConnection) {
+        const newPath = {
+          id: uuidv4(),
+          startElementId: state.currentConnection.startElementId,
+          startPort: state.currentConnection.startPort,
+          endElementId: action.payload.elementId,
+          endPort: action.payload.port,
+        };
+        state.paths.push(newPath);
+        state.currentConnection = null;
+        state.tempConnection = null;
+      }
+    },
+    cancelConnection: (state) => {
+      state.currentConnection = null;
+      state.tempConnection = null;
+    },
+    updateTempConnection: (state, action: PayloadAction<{ x: number; y: number }>) => {
+      state.tempConnection = { x: action.payload.x, y: action.payload.y };
+    },
   },
 });
 
-export const { addElement } = canvasSlice.actions;
+export const { addElement, startConnection, completeConnection, cancelConnection, updateTempConnection ,updateElementPosition} =
+  canvasSlice.actions;
 export default canvasSlice.reducer;
