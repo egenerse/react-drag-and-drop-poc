e4a9f6af127afd5e5622662770f51eabba347dc5
egenerse <egenerse@gmail.com>
2024-11-11 23:08:32 +0100

    feat: add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'react-drag-and-drop-poc',
+  webDir: 'dist'
+};
+
+export default config;
